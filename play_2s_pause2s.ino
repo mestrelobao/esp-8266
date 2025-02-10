dd9a6e5c201e8deedbca54344b02535103c500f6
mestrelobao <mestre_lobao@yahoo.com>
2025-02-10 15:55:11 -0300

    Create play_2s_pause2s.ino

---
+++ play_2s_pause2s.ino
@@ -0,0 +1,89 @@
+void setup()
+
+{
+
+    //Set up serial output with standard MIDI baud rate
+
+    Serial.begin(31250);
+
+    
+
+}
+
+
+
+void loop()
+
+{
+
+    //Play a G7M chord
+
+  
+  playMIDINote(1, 55, 100);
+  playMIDINote(1, 66, 100);
+  playMIDINote(1, 71, 100);
+  playMIDINote(1, 74, 100);
+
+        //Hold note for 4 seconds
+
+        delay(2000);
+
+        
+
+        //Turn chord off (velocity = 0)
+
+  playMIDINote(1, 55, 0);
+  playMIDINote(1, 66, 0);
+  playMIDINote(1, 71, 0);
+  playMIDINote(1, 74, 0);
+
+        //Pause for 4 seconds
+
+    delay(2000);
+  
+ //Play a D7M chord
+  
+  playMIDINote(1, 62, 100);
+  playMIDINote(1, 69, 100);
+  playMIDINote(1, 73, 100);
+  playMIDINote(1, 77, 100);
+
+        //Hold note for 4 seconds
+
+        delay(2000);
+
+        
+
+        //Turn chord off (velocity = 0)
+
+  playMIDINote(1, 62, 0);
+  playMIDINote(1, 69, 0);
+  playMIDINote(1, 73, 0);
+  playMIDINote(1, 79, 0);
+
+        //Pause for 4 seconds ms
+
+        delay(2000);
+}
+
+
+
+void playMIDINote(byte channel, byte note, byte velocity)
+
+{
+
+    //MIDI channels 1-16 are really 0-15
+
+    byte noteOnStatus=0x90 + (channel-1);  
+
+    
+
+    //Send notes to MIDI output:
+
+    Serial.write(noteOnStatus);
+
+    Serial.write(note);
+
+    Serial.write(velocity);
+
+}
