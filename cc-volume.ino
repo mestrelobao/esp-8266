a614b3f2cc828361ff6688a0cd81a5128c909327
mestrelobao <mestre_lobao@yahoo.com>
2025-02-11 23:14:37 -0300

    Create cc-volume.ino

---
+++ cc-volume.ino
@@ -0,0 +1,22 @@
+void setup()
+
+{
+    //Set up serial output with standard MIDI baud rate
+
+ Serial.begin(31250);
+
+ byte noteOnStatus=0xB0 + 0;  
+
+    Serial.write(noteOnStatus);
+
+    Serial.write(7);
+
+    Serial.write(10);
+  
+  }
+
+void loop()
+
+{
+ 
+}
